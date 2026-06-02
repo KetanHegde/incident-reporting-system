12fbcf402d1dd969633a86effd8ce7095928c124
KetanHegde <ketanhegde2003@gmail.com>
2026-06-02 16:27:05 +0530

    Set AWS region for DynamoDB resource

--- incident-backend/dynamo.py
+++ incident-backend/dynamo.py
@@ -1,7 +1,10 @@
 import boto3, os, uuid
 from datetime import datetime
 
-dynamo = boto3.resource("dynamodb")
+AWS_REGION = os.getenv("AWS_REGION", "ap-south-1")
+
+dynamo = boto3.resource("dynamodb", region_name=AWS_REGION)
+
 table = dynamo.Table(os.environ["DYNAMO_TABLE"])
 
 def log_event(event, user, incident_id):
